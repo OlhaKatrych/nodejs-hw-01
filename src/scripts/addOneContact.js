bbe5c58e5adfe993efcff58933571d9a61adc163
OlhaKatrych <olgak198931@gmail.com>
2024-07-14 17:20:40 +0300

    add one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import dataBase from '../db/db.json' assert { type: 'json' };
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  const oneContact = createFakeContact();
+  console.log(oneContact);
+  dataBase.push(oneContact);
+  console.log(dataBase);
+  await fs.writeFile(
+    PATH_DB,
+    JSON.stringify([...dataBase], null, 2),
+    {
+      encoding: 'utf-8',
+    },
+  );
+  return dataBase;
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import dataBase from '../db/db.json' assert { type: 'json' };
 
-const generateContacts = async (number) => {};
+export const generateContacts = async (number) => {
+  let contacts = [];
+  for (let i = 0; i < number; i++) {
+    contacts.push(createFakeContact());
+  }
 
-generateContacts(5);
+  dataBase.push(...contacts);
+  await fs.writeFile(PATH_DB, JSON.stringify(dataBase, null, 2), {
+    encoding: 'utf8',
+  });
+  return contacts;
+};
+generateContacts()
+  .then(() => fs.readFile(PATH_DB, { encoding: 'utf-8' }))
+  .catch((err) => console.error(err));
