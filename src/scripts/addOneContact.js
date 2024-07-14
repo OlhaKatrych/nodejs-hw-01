c579bc8bd0dfc71be97cd7b52beff4028b8a856a
OlhaKatrych <olgak198931@gmail.com>
2024-07-14 18:59:05 +0300

    del console.log

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -5,9 +5,7 @@ import dataBase from '../db/db.json' assert { type: 'json' };
 
 export const addOneContact = async () => {
   const oneContact = createFakeContact();
-  console.log(oneContact);
   dataBase.push(oneContact);
-  console.log(dataBase);
   await fs.writeFile(PATH_DB, JSON.stringify([...dataBase], null, 2), {
     encoding: 'utf-8',
   });
